c6a093d913eb456373fa6ecb9868b5092068075d
Enrique¨ <eterrrod@myuax.com>
2022-11-24 22:15:18 +0100

    enviando archivo

---
+++ sage2.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+floor(-3.56)
+
+
+# In[2]:
+
+
+ceil(-3.56)
+
+
+# In[3]:
+
+
+log(100,e)
+
+
+# In[4]:
+
+
+log(100,10)
+
+
+# In[5]:
+
+
+log(64,2)
+
+
+# In[6]:
+
+
+sin(pi/2)
+
+
+# In[7]:
+
+
+cot(pi/4)
+
+
+# In[8]:
+
+
+csc(pi/16)
+
+
+# In[ ]:
+
+
+
+
